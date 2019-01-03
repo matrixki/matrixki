6990d1e2a95b464d615f8270367a1688562b4f45
Mike Ko <minka1101@gmail.com>
2019-01-02 18:30:00 -0800

    added force https package

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HttpsRedirect from 'react-https-redirect';
 import './css/style.scss';
 import Index from './pages/index.js';
 import Blog from './pages/blog.js';
 import Post from './pages/post.js';
 
 const AppRouter = () => (
-    <Router>
-        <div>
-            <Switch>
-                <Route path="/" exact component={Index} />
-                <Route path="/blog" exact component={Blog} />
-                <Route path="/blog/:slug" component={Post} />
-            </Switch>
-        </div>
-    </Router>
+    <HttpsRedirect>
+        <Router>
+            <div>
+                <Switch>
+                    <Route path="/" exact component={Index} />
+                    <Route path="/blog" exact component={Blog} />
+                    <Route path="/blog/:slug" component={Post} />
+                </Switch>
+            </div>
+        </Router>
+    </HttpsRedirect>
 );
 
 export default AppRouter;
